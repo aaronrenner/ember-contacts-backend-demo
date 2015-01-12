a8ae74a3fd0421e43b53311305510ec2f8dc9ea4
Aaron Renner <aaron@animascodelabs.com>
2015-01-12 16:54:49 -0700

    Create and update contact endpoints

--- app/controllers/api/v1/contacts.rb
+++ app/controllers/api/v1/contacts.rb
@@ -14,8 +14,38 @@ module API
           requires :id, type: String, desc: "ID of the contact"
         end
         get ":id", root: :contact do
-          Contact.where(id: permitted_params[:id]).first!
+          Contact.find(permitted_params[:id])
         end
+
+        desc "Create a contact"
+        params do
+          requires :contact, type: Hash do
+            requires :first_name, type: String, desc: "First name of contact"
+            requires :last_name, type: String, desc: "Last name of contact"
+            requires :email, type: String, desc: "Email of contact"
+            requires :title, type: String, desc: "Title of contact"
+          end
+        end
+        post "", root: :contacts do
+          Contact.create!(permitted_params)
+        end
+
+        desc "Update a contact's attributes"
+        params do
+          requires :id, type: String, desc: "ID of the contact"
+          requires :contact, type: Hash do
+            optional :first_name, type: String, desc: "First name of contact"
+            optional :last_name, type: String, desc: "Last name of contact"
+            optional :email, type: String, desc: "Email of contact"
+            optional :title, type: String, desc: "Title of contact"
+          end
+        end
+        put ":id", root: :contact do
+          contact = Contact.find(permitted_params[:id])
+          contact.update_attributes! permitted_params[:contact]
+          contact
+        end
+
       end
     end
   end
